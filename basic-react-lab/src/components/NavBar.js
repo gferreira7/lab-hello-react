644b2bc6b8001e706db765b343821bc9403d787d
gferreira7 <104030930+gferreira7@users.noreply.github.com>
2023-03-28 20:45:35 +0100

    new proj created due to cloning error

---
+++ basic-react-lab/src/components/NavBar.js
@@ -0,0 +1,18 @@
+
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <img
+        src='./images/logo.png'
+        alt='logo'
+      />
+      <img
+        src='./images/hamburguer.png'
+        alt='burguer'
+      />
+    </div>
+  )
+}
+
+export default NavBar
